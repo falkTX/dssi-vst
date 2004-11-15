1aebb80de60d86fa68a8d5dd4c7c5359baf249bf
Chris Cannam <cannam@all-day-breakfast.com>
2004-11-15 08:57:17 +0000

    * add missing include

--- remotepluginclient.h
+++ remotepluginclient.h
@@ -12,6 +12,7 @@
 #include <string>
 #include <vector>
 #include <sys/shm.h>
+#include <errno.h>
 
 // Any of the methods in this file, including constructors, should be
 // considered capable of throwing RemotePluginClosedException.  Do not
